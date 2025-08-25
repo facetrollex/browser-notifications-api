1d3ede3111bf73550544934d4d63c35998ded3ff
Alexey Khamitsevich <alexey.khamitsevich@gmail.com>
2025-08-25 23:16:27 +0300

    Code cleanup / future work added in todos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-class BrowserNotificationAPI {
-    PERM_DEFAULT = 'default';
-    PERM_DENIED = 'denied';
-    PERM_GRANTED = 'granted';
+const PERM_DEFAULT = 'default';
+const PERM_DENIED = 'denied';
+const PERM_GRANTED = 'granted';
 
-    #permission = this.PERM_DEFAULT;
 
+//TODO - Implement This.
+const ASK_ON_CLASS_INIT = 'on_class_init';
+const ASK_ON_FIRST_NOTIFICATION = 'on_first_notification';
+const ASK_MANUAL = 'manual';
+class BrowserNotificationsAPI {
+    #permission = PERM_DEFAULT;
+    #isSupported = null;
+    #permissionsAsked = false;
     #config = {
-        requestOnInit: 1,
+        requestOnInit: 1, //Ask for Permissions on class init.
+        askPermissionsOn: 1, // TODO handle different types
+        askPermissionsOneTime: 1,
+        disableOnActiveWindow: true, //TODO: handle - Do not display notification once window is active
+        notificationOptions: { // TODO: handle this
+            title: 'Global Title',
+            body: 'Global Body',
+        }
     }
-    constructor() {
 
-        if(!this.#isSupported()) {
+    constructor() {
+        if(!this.isSupported) {
             throw new Error('Notifications not supported by browser.');
         }
 
@@ -26,35 +39,51 @@ class BrowserNotificationAPI {
     }
 
     set config(value) {
-        throw new Error('Property is read-only');
+        throw new Error('Config property is read-only.');
     }
 
-    #isSupported() {
-        return 'Notification' in window;
+    get isSupported() {
+        if(this.#isSupported == null) {
+            this.#isSupported = ('Notification' in window);
+        }
+        return this.#isSupported;
+    }
+
+    set isSupported(value) {
+        if(this.#isSupported === null) {
+            this.#isSupported = value; // Set only once.
+        }
     }
 
     showNotification(title, options) {
         let notification = null;
-        if(this.#permission === this.PERM_DENIED) {
+        if(this.#permission === PERM_DENIED) {
             throw Error('Notification permission denied');
         }
 
-        if(this.#permission === this.PERM_DEFAULT) {
+        if(this.#permission === PERM_DEFAULT) {
             //TODO:
             //Ask again?
         }
 
-        if(this.#permission === this.PERM_GRANTED) {
+        if(this.#permission === PERM_GRANTED) {
             notification = new Notification(title, options);
         }
 
         return notification;
     }
 
+    #handlePermissions() {
+        this.#permissionsAsked = true;
+    }
+
     #requestPermission() {
-        Notification.requestPermission().then((permission) => {
-            this.#permission = permission;
-        })
+        if(!this.#permissionsAsked) {
+            Notification.requestPermission().then((permission) => {
+                this.#permission = permission;
+                this.#permissionsAsked = true;
+            });
+        }
     }
 
     askForPermissions() {
@@ -62,4 +91,4 @@ class BrowserNotificationAPI {
     }
 }
 
-export default BrowserNotificationAPI;
+export default BrowserNotificationsAPI;
